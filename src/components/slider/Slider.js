0caa88b20ce4771270aa6b687ef0c6ae23eef10b
Arthur Ghazaryan <newartroll11@gmail.com>
2021-08-04 15:38:23 +0400

    add slider

--- src/components/slider/Slider.js
+++ src/components/slider/Slider.js
@@ -1,9 +1,25 @@
 import React from 'react'
+import ImageGallery from 'react-image-gallery';
+
+const images = [
+  {
+    original: 'https://picsum.photos/id/1018/1000/600/',
+    thumbnail: 'https://picsum.photos/id/1018/250/150/',
+  },
+  {
+    original: 'https://picsum.photos/id/1015/1000/600/',
+    thumbnail: 'https://picsum.photos/id/1015/250/150/',
+  },
+  {
+    original: 'https://picsum.photos/id/1019/1000/600/',
+    thumbnail: 'https://picsum.photos/id/1019/250/150/',
+  },
+];
 
 export default function Slider() {
     return (
         <div>
-           Slider 
+           <ImageGallery items={images} />
         </div>
     )
 }
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Header from '../../components/header/Header'
+import Slider from '../../components/slider/Slider'
 
 export default function Home() {
     return (
         <div>
             <Header />
+            <Slider />
         </div>
     )
 }
